e74986f322c1a9c7b119a2d609f050b8c7b19898
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-06-09 15:54:09 +0100

    Adding submission contstants
    Related Work Items: #1440

---
+++ ModernSlavery.PangolinTests/Modern Slavery Reporting UI Tests/Richer Sumbission/Submission_Constants.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modern_Slavery_Reporting_UI_Tests
+{
+    class Submission
+    {
+        public const string EmployerReference_Milbrook = "31QPYPBB";
+        public const string OrgName_Millbrook = "MILLBROOK HEALTHCARE LTD";
+        public const string CompanyNumber_Millbrook = "00833987";
+        public const string RegisteredAddress_Millbrook = "Nutsey Lane Calmore Ind Estate, Totton, Southampton, Hampshire, United Kingdom, SO40 3XJ";
+        public const string SecurtiyCode_Millbrook = "ABCD1234";
+
+        public const string EmployerReference_Blackpool = "NCSQN8T6";
+        public const string OrgName_Blackpool = "Blackpool Council";
+        public const string RegisteredAddress_Blackpool = "PO Box 4, Blackpool, FY1 1NA";
+        public const string Address1_Blackpool = "PO Box 4";
+        public const string Address2_Blackpool = "";
+        public const string Address3_Blackpool = "Blackpool";
+        public const string PostCode_Blackpool = "FY1 1NA";
+        //codes 1, 84110
+        public const string SicCodes_Blackpool = "Public Sector, General public administration activities";
+
+        public const string EmployerReference_InterFloor = "B72GL8R2";
+        public const string OrgName_InterFloor = "InterFloor Limited";
+        public const string RegisteredAddress_InterFloor = "Broadway, Haslingden, Rossendale, BB4 4LS";
+        public const string Address1_InterFloor = "Broadway, Haslingden";
+        public const string Address2_InterFloor = "";
+        public const string Address3_InterFloor = "Rossendale";
+        public const string PostCode_InterFloor = "BB4 4LS";
+        //codes 1, 84110
+        public const string SicCodes_InterFloor = "Public Sector, General public administration activities";
+
+
+
+    }
+}
