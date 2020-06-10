d792583f6654baa112b2bd39a37d33ee5ee3c20a
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-06-10 17:36:08 +0100

    Adding training page test cases and helper method
    Related Work Items: #1445, #2183

---
+++ ModernSlavery.PangolinTests/Modern Slavery Reporting UI Tests/Richer Sumbission/Submission_Helpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pangolin;
+using Pangolin.Helper.UIContext;
+namespace Modern_Slavery_Reporting_UI_Tests
+
+{
+    class Submission_Helper
+    {
+        public static void NavigateToTraining(UIContext ui, string Organistion, string Year)
+        {
+            ui.Click("Mange Organisations");
+            ui.ExpectHeader("Select an organisation");
+
+            ui.Click("Organisation");
+            ui.AtRow(Year).Click("Draft report");
+            ui.ExpectHeader("Your modern slavery statement");
+
+            ui.Click("Save and continue");
+            ui.ExpectHeader("Six areas of modern slavery statement");
+
+            ui.Click("Save and continue");
+            ui.ExpectHeader("Your organisation");
+
+            ui.Click("Save and continue");
+            ui.ExpectHeader("Supply chain risk");
+
+            ui.Click("Save and continue");
+            ui.ExpectHeader("Policies");
+
+            ui.Click("Save and continue");
+            ui.ExpectHeader("Due diligence");
+
+            ui.Click("Save and continue");
+            ui.ExpectHeader("Training");
+        }
+    }
+}
---
+++ ModernSlavery.PangolinTests/Modern Slavery Reporting UI Tests/Richer Sumbission/Submission_Training_Validation_Check.cs
@@ -0,0 +1,34 @@
+﻿using Pangolin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Modern_Slavery_Reporting_UI_Tests
+{
+    [TestClass]
+    public class Submission_Training_Validation_Check : UITest
+    {
+        [TestMethod]
+        public override void RunTest()
+        {
+            //Run<Fastrack_Registraion_Success>();
+
+            LoginAs<RogerReporter>();
+
+            Submission_Helper.NavigateToTraining(this, Submission.OrgName_Blackpool, "2019/2020");
+
+            ExpectHeader("Training");
+
+            //if chosing "other" details must be provided
+            ClickLabel("Other");
+            ClearField("Please specify");
+
+            Click("Save and continue");
+
+            Expect("There is a problem");
+            Expect("Please provide `other` details");
+
+            Set("Please specify").To("details");
+            Click("Save and continue");
+            ExpectHeader("Monitoring progress");
+        }
+    }
+}
