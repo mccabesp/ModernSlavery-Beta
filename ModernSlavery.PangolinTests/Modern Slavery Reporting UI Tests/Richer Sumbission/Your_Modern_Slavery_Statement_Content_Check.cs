0e4b3d8d54d5fdc65d7921c54c5ead14612c10f4
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-06-16 15:54:03 +0100

    Date input test cases
    Related Work Items: #2175

---
+++ ModernSlavery.PangolinTests/Modern Slavery Reporting UI Tests/Richer Sumbission/Your_Modern_Slavery_Statement_Content_Check.cs
@@ -0,0 +1,54 @@
+﻿using Pangolin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Modern_Slavery_Reporting_UI_Tests
+{
+    [TestClass]
+    public class Your_Modern_Slavery_Statement_Content_Check : UITest
+    {
+        [TestMethod]
+        public override void RunTest()
+        {
+            Run<Fastrack_Registraion_Success>();
+
+            LoginAs<RogerReporter>();
+
+            ExpectHeader("Select an organisation");
+
+            Click(Submission.OrgName_Blackpool);
+
+            ExpectHeader("Manage your organisations reporting");
+            Expect("Provide a link to the modern slavery statement on your organisation's website");
+
+            ExpectLink("More information");
+
+            ExpectField("Url");
+
+            //todo confirm format of day month year fields
+            AtLabel(The.Top, "Date").ExpectField("Day");
+            AtLabel(The.Top, "Date").ExpectField("Month");
+            AtLabel(The.Top, "Date").ExpectField("Year");
+
+            AtLabel("to").ExpectField("Day");
+            AtLabel("to").ExpectField("Month");
+            AtLabel("to").ExpectField("Year");
+
+            Expect("Who is the director (or equivalent) that signed off your statement?");
+
+            ExpectField("First name");
+            ExpectField("Last name");
+            ExpectField("Job title");
+
+            ExpectHeader("What date was your statement approved by the board or equivalent management body?");
+
+            AtLabel(The.Bottom, "Date").ExpectField("Day");
+            AtLabel(The.Bottom, "Date").ExpectField("Month");
+            AtLabel(The.Bottom, "Date").ExpectField("Year");
+
+
+            ExpectButton("Save and continue");
+
+            ExpectButton("Cancel");
+        }
+    }
+}
---
+++ ModernSlavery.PangolinTests/Modern Slavery Reporting UI Tests/Richer Sumbission/Your_Modern_Slavery_Statement_Date_Validation.cs
@@ -0,0 +1,40 @@
+﻿using Pangolin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Modern_Slavery_Reporting_UI_Tests
+{
+    [TestClass]
+    public class Your_Modern_Slavery_Statement_Date_Validation : UITest
+    {
+        [TestMethod]
+        public override void RunTest()
+        {
+            Run<Fastrack_Registraion_Success>();
+
+            LoginAs<RogerReporter>();
+
+            ExpectHeader("Select an organisation");
+
+            Click(Submission.OrgName_Blackpool);
+
+            ExpectHeader("Manage your organisations reporting");
+
+            //to after from
+            Submission_Helper.DateSet(this, "2", "2", "2020", "1");
+            Submission_Helper.DateSet(this, "2", "2", "2019", "2");
+
+            Click("Continue");
+            Expect("There is a problem");
+            RefreshPage();
+
+
+            //invalid format
+            Submission_Helper.DateSet(this, "22", "12", "2020", "3");
+            Click("Continue");
+            Expect("There is a problem");
+            Expect("Date format is incorrect");
+
+            RefreshPage();
+        }
+    }
+}
