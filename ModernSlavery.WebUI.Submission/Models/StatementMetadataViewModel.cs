4d7e1dcaa7ff5b72d8a6f8dc7d1ce055cab417fc
G\Charlotte.Winfield <charlotte.winfield@geeks.ltd.uk>
2020-06-17 09:10:48 +0100

    #2424 building views and viewmodel

--- ModernSlavery.WebUI.Submission/Models/StatementMetadataViewModel.cs
+++ ModernSlavery.WebUI.Submission/Models/StatementMetadataViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
 using ModernSlavery.Core.Entities;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ModernSlavery.WebUI.Submission.Presenters
 {
@@ -21,11 +22,24 @@ namespace ModernSlavery.WebUI.Submission.Presenters
         // Earliest date that the submission can be started
         public DateTime ReportingStartDate { get; set; }
 
+        public DateTime ReportingEndDate { get; set; }
+        public DateTime AccountingDate { get; set; }
         // This should never go over the wire!
         public long OrganisationId { get; set; }
 
         public string OrganisationIdentifier { get; set; }
 
         public int Year { get; set; }
+        [Url]
+        [Display(Name = "URL")]
+        public string StatementUrl { get; set; }
+        [Display(Name = "Job Title")]
+        public string JobTitle { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public DateTime ApprovedDate { get; set; }
+
     }
 }
