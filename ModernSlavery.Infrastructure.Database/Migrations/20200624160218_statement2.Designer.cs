e8ac1aefdb29baff3feb664ada18b99481c60b09
James Lustig <james.lustig@geeks.ltd.uk>
2020-06-24 17:07:35 +0100

    Add migration for column rename

--- ModernSlavery.Infrastructure.Database/Migrations/20200624160218_statement2.Designer.cs
+++ ModernSlavery.Infrastructure.Database/Migrations/20200624160218_statement2.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ModernSlavery.Infrastructure.Database;
 
 namespace ModernSlavery.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200624160218_statement2")]
+    partial class statement2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
