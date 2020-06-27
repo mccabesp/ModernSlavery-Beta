0694e3c69f12899c2cbe43d3a94aef9cc193ebc0
Stephen McCabe <stephen.mccabe@cadenceinnova.com>
2020-06-27 18:59:47 +0100

    Moved all static files to assets folder for better serving from CDN

---
+++ ModernSlavery.WebUI.Shared/Options/CdnOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ModernSlavery.Core.Attributes;
+using ModernSlavery.Core.Extensions;
+using ModernSlavery.Core.Options;
+
+namespace ModernSlavery.WebUI.Shared.Options
+{
+    [Options("CDN")]
+    public class CdnOptions: IOptions
+    {
+        private string _Endpoint;
+        public string Endpoint { get=>_Endpoint; set=> _Endpoint= value==null || !value.StartsWithI("http") ? value?.Trim(' ', '/', '\\', ':') : $"https://{value.Trim(' ', '/', '\\', ':')}";}
+
+        public string GetEndpoint(string relativePath)
+        {
+            if (string.IsNullOrWhiteSpace(relativePath)) throw new NullReferenceException(nameof(relativePath));
+            if (string.IsNullOrWhiteSpace(Endpoint)) return relativePath;
+            return $"{Endpoint}/{relativePath.Trim(' ', '/', '\\', ':')}";
+        }
+
+        public string GetFallback(string relativePath)
+        {
+            if (string.IsNullOrWhiteSpace(relativePath)) throw new NullReferenceException(nameof(relativePath));
+            if (!string.IsNullOrWhiteSpace(Endpoint)) return relativePath;
+            return null;
+        }
+    }
+}
