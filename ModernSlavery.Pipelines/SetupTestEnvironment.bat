c23cceedd7939531a01d9284304f20f5a71a4d1a
Stephen McCabe <stephen.mccabe@cadenceinnova.com>
2020-07-04 13:29:38 +0100

    Addede batch file to setup test environment

---
+++ ModernSlavery.Pipelines/SetupTestEnvironment.bat
@@ -0,0 +1,7 @@
+@echo off
+set ClientId=%~1
+set ClientSecret=%~2
+SET ASPNETCORE_ENVIRONMENT=%~3
+@echo ClientId=%ClientId%
+@echo ClientSecret=%ClientSecret%
+@echo ASPNETCORE_ENVIRONMENT=%ASPNETCORE_ENVIRONMENT%
