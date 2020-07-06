0d24281eae0e227f04b51ec8511a286ad7f55db9
James Lustig <james.lustig@geeks.ltd.uk>
2020-07-06 09:57:45 +0100

    Begin draft file handling for statements

--- ModernSlavery.BusinessDomain.Submission/StatementBusinessLogic.cs
+++ ModernSlavery.BusinessDomain.Submission/StatementBusinessLogic.cs
@@ -2,9 +2,11 @@
 using ModernSlavery.BusinessDomain.Shared.Interfaces;
 using ModernSlavery.Core.Entities;
 using ModernSlavery.Core.Extensions;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -23,9 +25,22 @@ namespace ModernSlavery.BusinessDomain.Submission
 
         public async Task<Statement> GetStatementByOrganisationAndYear(Organisation organisation, int year)
         {
-            return await SharedBusinessLogic.DataRepository
+            var dataResult = await SharedBusinessLogic.DataRepository
                 // Is the accounting year the correct year?
                 .FirstOrDefaultAsync<Statement>(s => s.OrganisationId == organisation.OrganisationId && s.SubmissionDeadline.Year == year);
+
+            if (dataResult != null)
+                return dataResult;
+
+            var path = GetFileName(organisation.OrganisationId, year);
+            if (!await SharedBusinessLogic.FileRepository.GetFileExistsAsync(path))
+                return null;
+
+            var content = await SharedBusinessLogic.FileRepository.ReadAsync(path);
+
+            var fileResult = JsonConvert.DeserializeObject<Statement>(content);
+
+            return fileResult;
         }
 
         public async Task<StatementActionResult> CanAccessStatement(User user, Organisation organisation, int reportingYear)
@@ -46,11 +61,34 @@ namespace ModernSlavery.BusinessDomain.Submission
             return StatementActionResult.Success;
         }
 
+        private async Task SaveToFile(Organisation organisation, Statement statement)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                DefaultValueHandling = DefaultValueHandling.Ignore,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+            var data = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(statement, Formatting.Indented, settings));
+            var fileName = GetFileName(organisation.OrganisationId, statement.SubmissionDeadline.Year);
+            await SharedBusinessLogic.FileRepository.WriteAsync(fileName, data);
+        }
+
+        private string GetFileName(long organisationId, int reportingYear)
+            => $"{organisationId}_{reportingYear}.json";
+
         public async Task<StatementActionResult> SaveStatement(User user, Organisation organisation, Statement statement)
         {
             if (!statement.CanBeEdited)
             {
                 return StatementActionResult.Uneditable;
+            }
+
+            if (statement.Status == ReturnStatuses.Draft)
+            {
+                await SaveToFile(organisation, statement);
+
+                return StatementActionResult.Success;
             }
 
             // Is this check enough?
