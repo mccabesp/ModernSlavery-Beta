96e198a401b87ac8f3b221bb0b21fdbd99aa31aa
Stephen McCabe <stephen.mccabe@cadenceinnova.com>
2020-07-17 16:51:24 +0100

    Changed migrations to not run on Production and only when enabled and app is Hosts.Web

--- ModernSlavery.Infrastructure.Database/DependencyModule.cs
+++ ModernSlavery.Infrastructure.Database/DependencyModule.cs
@@ -17,13 +17,16 @@ namespace ModernSlavery.Infrastructure.Database
 
         private readonly ILogger _logger;
         private readonly SharedOptions _sharedOptions;
+        private readonly DatabaseOptions _databaseOptions;
 
         public DependencyModule(
             ILogger<DependencyModule> logger,
+            DatabaseOptions databaseOptions,
             SharedOptions sharedOptions)
         {
             //Set any required local IOptions here
             _logger = logger;
+            _databaseOptions = databaseOptions;
             _sharedOptions = sharedOptions;
         }
 
@@ -45,34 +48,38 @@ namespace ModernSlavery.Infrastructure.Database
 
         public void Configure(ILifetimeScope lifetimeScope)
         {
-            //Ensure import files exist on remote storage
-            var fileRepository = lifetimeScope.Resolve<IFileRepository>();
-            Task.WaitAll(
-                fileRepository.PushRemoteFileAsync(Filenames.ShortCodes, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.SicCodes, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.SicSections, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.StatementDiligenceTypes, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.StatementPolicyTypes, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.StatementRiskTypes, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.StatementSectorTypes, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.StatementTrainingTypes, _sharedOptions.DataPath),
-                fileRepository.PushRemoteFileAsync(Filenames.ImportOrganisations, _sharedOptions.DataPath)
-            );
-
-            //Seed database whenver migrations are applied
-            var dbContext = lifetimeScope.Resolve<IDbContext>();
-            if (dbContext.MigrationsApplied)
+            //Only when migrations is enabled
+            if (_databaseOptions.UseMigrations)
             {
-                var _dataImporter = lifetimeScope.Resolve<IDataImporter>();
+                //Ensure import files exist on remote storage
+                var fileRepository = lifetimeScope.Resolve<IFileRepository>();
+                Task.WaitAll(
+                    fileRepository.PushRemoteFileAsync(Filenames.ShortCodes, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.SicCodes, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.SicSections, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.StatementDiligenceTypes, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.StatementPolicyTypes, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.StatementRiskTypes, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.StatementSectorTypes, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.StatementTrainingTypes, _sharedOptions.DataPath),
+                    fileRepository.PushRemoteFileAsync(Filenames.ImportOrganisations, _sharedOptions.DataPath)
+                );
 
-                _dataImporter.ImportOrganisationsAsync().Wait();
-                _dataImporter.ImportSICCodesAsync().Wait();
-                _dataImporter.ImportSICSectionsAsync().Wait();
-                _dataImporter.ImportStatementDiligenceTypesAsync().Wait();
-                _dataImporter.ImportStatementPolicyTypesAsync().Wait();
-                _dataImporter.ImportStatementRiskTypesAsync().Wait();
-                _dataImporter.ImportStatementSectorTypesAsync().Wait();
-                _dataImporter.ImportStatementTrainingTypesAsync().Wait();
+                //Seed database whenver migrations are applied
+                var dbContext = lifetimeScope.Resolve<IDbContext>();
+                if (dbContext.MigrationsApplied)
+                {
+                    var _dataImporter = lifetimeScope.Resolve<IDataImporter>();
+
+                    _dataImporter.ImportOrganisationsAsync().Wait();
+                    _dataImporter.ImportSICCodesAsync().Wait();
+                    _dataImporter.ImportSICSectionsAsync().Wait();
+                    _dataImporter.ImportStatementDiligenceTypesAsync().Wait();
+                    _dataImporter.ImportStatementPolicyTypesAsync().Wait();
+                    _dataImporter.ImportStatementRiskTypesAsync().Wait();
+                    _dataImporter.ImportStatementSectorTypesAsync().Wait();
+                    _dataImporter.ImportStatementTrainingTypesAsync().Wait();
+                }
             }
         }
 
