ded036ef0332104d42665c29a928036bc4869a79
Stephen McCabe <stephen.mccabe@cadenceinnova.com>
2020-07-20 18:29:27 +0100

    Added new Generic Outcome helper

---
+++ ModernSlavery.Core/Classes/ErrorMessages/Outcome.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModernSlavery.Core.Classes.ErrorMessages
+{
+    [Serializable]
+    public class Outcome<TFail> 
+    {
+        public Outcome()
+        {
+        }
+
+        public Outcome(TFail error, string message=null)
+        {
+            Errors.Add((error, message));
+        }
+
+        public List<(TFail Error, string Message)> Errors { get; set; } = new List<(TFail, string)>();
+        public bool Success => !Fail;
+        public bool Fail => Errors != null && Errors.Any();
+    }
+
+    [Serializable]
+    public class Outcome<TFail, TSuccess>:Outcome<TFail>
+    {
+        public Outcome(TSuccess result):base()
+        {
+            Result = result;
+        }
+
+        public Outcome(TFail error, string message = null):base(error,message)
+        {
+        }
+
+        public TSuccess Result { get; }
+    }
+}
