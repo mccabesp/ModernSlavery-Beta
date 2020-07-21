c1e42abfc71a0ec4072d2aa9a22eada9fe792271
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-07-21 15:55:55 +0100

    Rename test case and update registration test case
    Related Work Items: #2988

---
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Registration/Public/Public_Registration_Success.cs
@@ -0,0 +1,124 @@
+﻿using Geeks.Pangolin;
+using ModernSlavery.Core.Extensions;
+using ModernSlavery.Core.Interfaces;
+using ModernSlavery.Infrastructure.Hosts;
+using ModernSlavery.Testing.Helpers;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using static ModernSlavery.Core.Extensions.Web;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using ModernSlavery.Core.Entities;
+
+
+namespace ModernSlavery.Hosts.Web.Tests
+{
+    [TestFixture]
+    public class Public_Registration_Success : CreateAccount
+    {
+        const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
+
+        string Pin;
+        public Public_Registration_Success() : base(_firstname, _lastname, _title, _email, _password)
+        {
+
+
+        }
+
+        [Test, Order(20)]
+        public async Task GoToPublicRegistrationPage()
+        {
+
+            Click("Register an organisation");
+
+
+            ExpectHeader("Registration Options");
+
+            ClickLabel(That.Contains, "Private limited company");
+            Click("Continue");
+
+            ExpectHeader("Find your organisation");
+            await Task.CompletedTask;
+
+        }
+
+        [Test, Order(21)]
+
+        public async Task SearchForOrganisation()
+        { 
+            SetXPath("//*[@id='SearchText']").To(Registration.OrgName_InterFloor);
+            Click("Search");
+
+            ExpectRow(That.Contains, Registration.OrgName_InterFloor);
+
+            await Task.CompletedTask;
+
+        }
+        [Test, Order(22)]
+
+        public async Task ChooseOrganisation()
+        { 
+        AtRow(That.Contains, Registration.OrgName_InterFloor).Click(What.Contains, "Choose");
+
+            ExpectHeader("Confirm your organisation’s details");
+
+            AtRow("Organisation name").Expect(Registration.OrgName_InterFloor);
+            AtRow("Registered address").Expect(Registration.RegisteredAddress_InterFloor);
+            Click("Confirm");
+
+            ExpectHeader("We're sending a PIN by post to the following name & address:");
+            await Task.CompletedTask;
+
+        }
+        [Test, Order(23)]
+
+        public async Task ExtractPin() {
+            Pin = WebDriver.FindElement(By.XPath("//b")).Text;
+            await Task.CompletedTask;
+        }
+
+        [Test, Order(24)]
+
+        public async Task ExpectOrgDetails()
+        {
+
+            Expect(What.Contains, Create_Account.roger_first + " " + Create_Account.roger_last + " (" + Create_Account.roger_job_title + ")");
+            Expect(What.Contains, Registration.Address1_InterFloor);
+            Expect(What.Contains, Registration.Address3_InterFloor);
+            Expect(What.Contains, Registration.PostCode_InterFloor);
+
+            await Task.CompletedTask;
+
+        }
+        [Test, Order(25)]
+
+        public async Task VerifyPin()
+        {
+
+            Goto("/");
+            Click("Manage Organisations");
+            ExpectHeader("Select an organisation");
+
+
+            ClickText(Registration.OrgName_InterFloor);
+
+            ExpectHeader("Please enter your Modern Slavery Reporting service PIN");
+
+            Expect("Enter the PIN to enable you to report Modern Slavery information for this organisation.");
+
+            Set("Enter PIN").To(Pin);
+
+            ClickText("Activate and continue");
+
+            AtRow(Registration.OrgName_InterFloor).Expect("Registration complete");
+
+            await Task.CompletedTask;
+
+        }
+
+
+    }
+}
