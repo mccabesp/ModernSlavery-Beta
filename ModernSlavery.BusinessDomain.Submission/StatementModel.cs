bc3c011b0e9e532282939af255570c0a50e6bdd7
Stephen McCabe <stephen.mccabe@cadenceinnova.com>
2020-07-21 17:39:32 +0100

    Moved StatementMapperProfile inside StatementModel

--- ModernSlavery.BusinessDomain.Submission/StatementModel.cs
+++ ModernSlavery.BusinessDomain.Submission/StatementModel.cs
@@ -1,13 +1,30 @@
-﻿using ModernSlavery.Core.Entities;
+﻿using AutoMapper;
+using ModernSlavery.Core.Entities;
 using ModernSlavery.Core.Extensions;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+using System.Linq;
 
 namespace ModernSlavery.BusinessDomain.Submission
 {
+    public class StatementMapperProfile : Profile
+    {
+        public StatementMapperProfile()
+        {
+            // name things same as DB
+            CreateMap<Statement, StatementModel>()
+                .ForMember(dest => dest.Status, opt => opt.Ignore()) // TODO - James Map this appropriately
+                .ForMember(dest => dest.Year, opt => opt.MapFrom(src => src.SubmissionDeadline.Year))
+                .ForMember(dest => dest.StatementSectors, opt => opt.MapFrom(src => src.Sectors.Select(s => s.StatementSectorTypeId)))
+                .ForMember(dest => dest.StatementPolicies, opt => opt.MapFrom(src => src.Policies.Select(p => p.StatementPolicyTypeId)))
+                .ForMember(dest => dest.Training, opt => opt.MapFrom(src => src.Training.Select(t => t.StatementTrainingTypeId)))
+                .ForMember(dest => dest.RelevantRisks, opt => opt.MapFrom(src => src.RelevantRisks.Select(r => r.StatementRiskTypeId)))
+                .ForMember(dest => dest.HighRisks, opt => opt.MapFrom(src => src.HighRisks.Select(r => r.StatementRiskTypeId)))
+                .ForMember(dest => dest.LocationRisks, opt => opt.MapFrom(src => src.LocationRisks.Select(r => r.StatementRiskTypeId)))
+                .ForMember(dest => dest.Diligences, opt => opt.MapFrom(src => src.Diligences.Select(d => d.StatementDiligenceTypeId)));
+        }
+    }
+
     [Serializable]
     public class StatementModel
     {
@@ -15,6 +32,7 @@ namespace ModernSlavery.BusinessDomain.Submission
         public DateTime Timestamp { get; set; }
 
         public long? StatementId { get; set; }
+        public DateTime? BackupDate { get; set; }
 
         public StatementStatuses Status { get; set; }
 
