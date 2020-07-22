a3fdaa4b742c79c7d8b0814dd7ab3ff6fe5dacaa
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-07-22 13:42:51 +0100

    Adding close account and fastrack test cases

---
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Manage Account/Closed Account Cannot Sign In.cs
@@ -0,0 +1,43 @@
+﻿using Geeks.Pangolin;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace ModernSlavery.Hosts.Web.Tests
+{
+    public class ClosedAccountCannotSignIn : CloseAccount
+    {
+        const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
+
+        [Test, Order(20)]
+
+        public async Task Logout()
+        {
+            Logout();
+
+            Goto("/");
+            await Task.CompletedTask;
+        }
+        [Test, Order(22)]
+
+        public async Task FillInUserNameAndPassword() { 
+            Goto("/");
+        Click("Sign in");
+        Set("Email").To(Create_Account.roger_email);
+        Set("Password").To(Create_Account.roger_password);
+        await Task.CompletedTask;
+    }
+        [Test, Order(24)]
+
+        public async Task ClickingSignInLeadsToError()
+        {
+
+            Click(The.Bottom, "Sign in");
+            Expect("There`s a problem with your email address or password");
+            await Task.CompletedTask;
+
+        }
+
+
+
+    }
+}
---
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Registration/Fastrack/Fastrack_Registration_Invalid_Reference.cs
@@ -0,0 +1,70 @@
+﻿using Geeks.Pangolin;
+using ModernSlavery.Core.Extensions;
+using ModernSlavery.Core.Interfaces;
+using ModernSlavery.Infrastructure.Hosts;
+using ModernSlavery.Testing.Helpers;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using static ModernSlavery.Core.Extensions.Web;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using ModernSlavery.Core.Entities;
+
+
+namespace ModernSlavery.Hosts.Web.Tests
+{
+    [TestFixture]
+    public class Fastrack_Registration_Invalid_Reference : CreateAccount
+    {
+        const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
+
+        public Fastrack_Registration_Invalid_Reference() : base(_firstname, _lastname, _title, _email, _password)
+        {
+
+
+        }
+
+        [Test, Order(20)]
+        public async Task GoToRegistrationPage()
+        {
+            Click("Register an organisation");
+
+
+            ExpectHeader("Registration Options");
+            await Task.CompletedTask;
+
+        }
+        [Test, Order(21)]
+
+        public async Task EnterInvalidDetails()
+        {
+            ClickLabel("Fast Track");
+            Click("Continue");
+
+            ExpectHeader("Fast track registration");
+
+            //todo ensure valid security code added here
+            Set("Employer reference").To(Registration.InvalidEmployerReference);
+            Set("Security code").To(Registration.ValidSecurityCode);
+            await Task.CompletedTask;
+        }
+
+        [Test, Order(22)]
+
+        public async Task SubmittingFormReturnsValidationMessage()
+        {
+            Click("Continue");
+
+            ExpectHeader("There is a problem");
+            Expect("There's a problem with your employer reference or security code");
+            await Task.CompletedTask;
+        }
+
+        }
+
+        
+
+    }
