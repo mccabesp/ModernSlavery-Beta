45df865e277523d539d8339a6dfa923523512297
Stephen McCabe <stephen.mccabe@cadenceinnova.com>
2020-07-27 15:49:54 +0100

    Started adding Obfuscate filter and Deobfuscate modelbinder

---
+++ ModernSlavery.WebUI.Shared/Classes/Binding/ObfuscateAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ModernSlavery.WebUI.Shared.Classes.Attributes
+{
+    public interface IObfuscatedAttribute { }
+
+    public class ObfuscateAttribute
+        : Attribute, IObfuscatedAttribute
+    { }
+}
