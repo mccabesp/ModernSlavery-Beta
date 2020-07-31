03f7af06f40ab66123d51c9f8d130cf466eaaf86
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-07-31 08:48:13 +0100

    Add missing ignore trait

--- ModernSlavery.Hosts.Web.Tests/Functional tests/Manage Account/Closed Account Cannot Sign In.cs
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Manage Account/Closed Account Cannot Sign In.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 
 namespace ModernSlavery.Hosts.Web.Tests
 {
+    [TestFixture, Ignore("Awaiting fix for close account")]
     public class ClosedAccountCannotSignIn : CloseAccount
     {
         const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
