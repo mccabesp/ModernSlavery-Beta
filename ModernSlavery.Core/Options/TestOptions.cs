8ceedfdddb08d33ab49f7a6796d6f651c3e9baea
Stephen McCabe <stephen.mccabe@cadenceinnova.com>
2020-08-07 13:27:37 +0100

    Added new TestOptions to force Google and Appinsights telemetry on even when cookies are off

---
+++ ModernSlavery.Core/Options/TestOptions.cs
@@ -0,0 +1,10 @@
+﻿using ModernSlavery.Core.Attributes;
+
+namespace ModernSlavery.Core.Options
+{
+    [Options("Email")]
+    public class EmailOptions : IOptions
+    {
+        public string AdminDistributionList { get; set; }
+    }
+}
