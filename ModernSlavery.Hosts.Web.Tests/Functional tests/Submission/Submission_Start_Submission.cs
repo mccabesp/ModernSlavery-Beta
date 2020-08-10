08730862bf3c6334abdafb66f3dc6f2b4b30226f
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-08-10 09:30:29 +0100

    Adding start submission test as starting point for other submission test cases

---
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Submission/Submission_Start_Submission.cs
@@ -0,0 +1,31 @@
+﻿using Geeks.Pangolin;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace ModernSlavery.Hosts.Web.Tests
+{
+    [TestFixture]
+
+    public class Submission_Start_Submission : Private_Registration_Success
+    {
+        [Test, Order(40)]
+        public async Task StartSubmission()
+        {
+            ExpectHeader("Select an organisation");
+
+            Click(Submission.OrgName_InterFloor);
+
+
+            ExpectHeader(That.Contains, "Manage your modern slavery statement submissions");
+
+            Click("Start Draft");
+
+
+            ExpectHeader("Before you start");
+            Click("Start now");
+
+            ExpectHeader("Your modern slavery statement");
+            await Task.CompletedTask;
+        }
+    }
+}
