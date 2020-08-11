f06cc7f26ccaa057a4da6c1325f1b92e79c7a458
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-08-11 09:16:22 +0100

    Adding scope constants file

---
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Scope Out/Scope Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernSlavery.Hosts.Web.Tests
+{
+    class ScopeConstants
+    {
+        //todo enter scope url
+        public const string ScopeUrl = "";
+       
+    }
+}
