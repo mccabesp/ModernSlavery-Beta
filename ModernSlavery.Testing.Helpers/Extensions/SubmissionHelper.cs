afa0fec94703645ccda330d01fdd3b82baa7030e
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-09-10 09:02:46 +0100

    Adding submission helper

---
+++ ModernSlavery.Testing.Helpers/Extensions/SubmissionHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Geeks.Pangolin;
+using Geeks.Pangolin.Core.Helper;
+using Geeks.Pangolin.Helper.UIContext;
+
+namespace ModernSlavery.Testing.Helpers.Extensions
+{
+    class SubmissionHelper
+    {
+        public static void GroupOrSingleScreenComplete(UIContext ui, bool IsGroup = false, string OrgName = null, string Year = null)
+        {
+            ui.ExpectHeader("Who is your statement for?");
+
+            if (OrgName != null && Year != null)
+            {
+                ui.ExpectHeader("The " + Year + " modern slavery statement for " + OrgName + " covers:");
+            }
+
+            if (IsGroup)
+            {
+                ui.ClickLabel("a group of organisations");
+                ui.Click("Continue");
+                ui.ExpectHeader("Which organisations are included in your group statement?");
+            }
+            else
+            {
+                ui.ClickLabel("a single organisation");
+                ui.Click("Continue");
+                ui.ExpectHeader("Your modern slavery statement");
+            }
+        }
+    }
+}
