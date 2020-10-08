3cbf0a8cc5b139f3078da8e2e9a7e79d555b0e9d
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-10-08 14:10:18 +0100

    Public test case fixes

--- ModernSlavery.Hosts.Web.Tests/Functional tests/Registration/Public/Registration_Public_Failure_Already_Started.cs
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Registration/Public/Registration_Public_Failure_Already_Started.cs
@@ -22,7 +22,7 @@ namespace ModernSlavery.Hosts.Web.Tests
     {
         const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
 
-        [Test, Order(30)]
+        [Test, Order(40)]
         public async Task RegisterDuplicateOrg()
         {
             Goto("/");
@@ -31,7 +31,7 @@ namespace ModernSlavery.Hosts.Web.Tests
 
             await Task.CompletedTask;
         }
-        [Test, Order(32)]
+        [Test, Order(42)]
 
         public async Task SelectingDuplicateOrgCausesValidation()
         {
--- ModernSlavery.Hosts.Web.Tests/Functional tests/Registration/Public/Registration_Public_Failure_Duplicate_Organisation.cs
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Registration/Public/Registration_Public_Failure_Duplicate_Organisation.cs
@@ -1,4 +1,7 @@
 ﻿using Geeks.Pangolin;
+using ModernSlavery.Core.Entities;
+using ModernSlavery.Core.Extensions;
+using ModernSlavery.Testing.Helpers.Extensions;
 using NUnit.Framework;
 using System.Threading.Tasks;
 
@@ -9,6 +12,16 @@ namespace ModernSlavery.Hosts.Web.Tests
     public class Registration_Public_Failure_Duplicate_Organisation : Registration_Public_Success
     {
         const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
+
+        private Organisation org;
+
+        [OneTimeSetUp]
+        public async Task OTSetUp()
+        {
+            //HostHelper.ResetDbScope();
+            org = this.Find<Organisation>(org => org.LatestRegistrationUserId == null && org.SectorType.IsAny(SectorTypes.Public));
+            await Task.CompletedTask;
+        }
 
         [Test, Order(40)]
         public async Task SearchForDuplicateOrg()
