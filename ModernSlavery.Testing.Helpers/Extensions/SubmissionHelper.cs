83e6407f4b2a29d80200c4c3bc6e29d3061c8428
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-10-08 09:57:37 +0100

    Add missing helper

--- ModernSlavery.Testing.Helpers/Extensions/SubmissionHelper.cs
+++ ModernSlavery.Testing.Helpers/Extensions/SubmissionHelper.cs
@@ -32,5 +32,42 @@ namespace ModernSlavery.Testing.Helpers.Extensions
                 ui.ExpectHeader("Your modern slavery statement");
             }
         }
+
+        public static void MoreInformationRequiredComplete(UIContext ui, bool WasRequired, string DeadlineYear = "2019", string OrgName = null)
+        {
+
+         ui.Expect("We need more information");
+            if (OrgName.HasValue())
+            {
+                ui.Expect(OrgName);
+            }
+
+            ui.Expect("Was your organisation required to report for reporting deadline 31 December " + DeadlineYear + "?");
+
+            if (WasRequired)
+            {
+                ui.ClickLabel("Yes");
+            }
+            else
+            {
+                ui.ClickLabel("No");
+            }
+
+            ui.Expect(What.Contains, "If you don't know if your organisation is required to report please read the ");
+            ui.ExpectLink(That.Contains, "guidance");
+
+            ui.Click("Continue");
+
+            ui.ExpectText(That.Contains, "Press continue to manage your organisation.");
+            if (OrgName.HasValue())
+            {
+                ui.ExpectHeader(OrgName);
+            }
+
+            //ui.Expect(What.Contains, "You've confirmed your organisation is required to report for reporting deadline 31 December " + DeadlineYear + ".");
+
+            ui.Click("Continue");
+            ui.ExpectHeader(That.Contains, "Manage your modern slavery statement submissions");
+        }
     }
 }
