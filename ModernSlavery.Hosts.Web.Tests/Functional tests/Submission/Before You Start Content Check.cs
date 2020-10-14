6faf8e85cb34d1136c600909cdf78f50a994017d
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-10-14 14:03:11 +0100

    Remove ignore and fix test case

--- ModernSlavery.Hosts.Web.Tests/Functional tests/Submission/Before You Start Content Check.cs
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Submission/Before You Start Content Check.cs
@@ -1,28 +1,55 @@
 ﻿using Geeks.Pangolin;
+using ModernSlavery.Core.Entities;
+using ModernSlavery.Core.Extensions;
 using ModernSlavery.Testing.Helpers.Extensions;
 using NUnit.Framework;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ModernSlavery.Hosts.Web.Tests
 {
-    [TestFixture, Ignore("Awaiting Submission merge")]
 
-    public class Before_You_Start_Content_Check : Private_Registration_Success
+    public class Before_You_Start_Content_Check : CreateAccount
     {
-        [Test, Order(40)]
+       const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
+
+
+            private Organisation org;
+            [OneTimeSetUp]
+            public async Task SetUp()
+            {
+                org = this.Find<Organisation>(org => org.GetLatestActiveScope().ScopeStatus.IsAny(ScopeStatuses.PresumedOutOfScope, ScopeStatuses.PresumedInScope) && org.LatestRegistrationUserId == null && !org.UserOrganisations.Any());
+            await Task.CompletedTask;
+            }
+
+            public Before_You_Start_Content_Check() : base(_firstname, _lastname, _title, _email, _password)
+            {
+
+
+            }
+
+            [Test, Order(29)]
+            public async Task RegisterOrg()
+            {
+                await this.RegisterUserOrganisationAsync(org.OrganisationName, UniqueEmail);
+            }
+
+
+            [Test, Order(40)]
         public async Task NavigateToBeforeYouStart()
         {
 
+            RefreshPage();
+
             ExpectHeader("Select an organisation");
 
-            Click(Submission.OrgName_Blackpool);
+            Click(org.OrganisationName);
             await AxeHelper.CheckAccessibilityAsync(this);
-            ExpectHeader("Manage your organisations reporting");
+            SubmissionHelper.MoreInformationRequiredComplete(this, true, OrgName: org.OrganisationName);
 
-            AtRow("2019/20").Click("Draft report");
-            await AxeHelper.CheckAccessibilityAsync(this, httpMethod: "POST");
+            ExpectHeader(That.Contains, "Manage your modern slavery statement submissions");
 
-            ExpectHeader("Before you start");
+            Click(The.Bottom, "Start Draft");
 
 
             await Task.CompletedTask;
@@ -47,7 +74,7 @@ namespace ModernSlavery.Hosts.Web.Tests
         [Test, Order(43)]
         public async Task ExpandFirstParagraphAndCheckContent()
         {
-            Click("Read more about the information you need to provide");
+            ClickText("Read more about the information you need to provide");
             await AxeHelper.CheckAccessibilityAsync(this);
             Expect("the name of the organisation, or group of organisations, your statement is for");
             Expect("the period covered by the statement");
@@ -61,7 +88,7 @@ namespace ModernSlavery.Hosts.Web.Tests
         [Test, Order(44)]
         public async Task SecondParagraphNotExpanded()
         {
-            Expect("We will then ask you some additional questions about your statement.These are optional – but we strongly encourage you to complete them.");
+            Expect("We will then ask you some additional questions about your statement. These are optional – but we strongly encourage you to complete them.");
             Expect("Read more about the additional questions");
             ExpectNo("Additional questions cover:");
 
@@ -71,7 +98,7 @@ namespace ModernSlavery.Hosts.Web.Tests
         [Test, Order(45)]
         public async Task ExpandSecondParagraphForContentCheck()
         {
-            Click("Read more about the additional questions");
+            ClickText("Read more about the additional questions");
             Expect("Additional questions cover:");
             Expect("the sector your organisation operates in");
             Expect("your policies and codes in relation to modern slavery");
@@ -90,7 +117,7 @@ namespace ModernSlavery.Hosts.Web.Tests
             Expect("Start now");
             Click("Start now");
             await AxeHelper.CheckAccessibilityAsync(this);
-            ExpectHeader("Who is this statement for?");
+            ExpectHeader("Who is your statement for?");
 
             await Task.CompletedTask;
         }
