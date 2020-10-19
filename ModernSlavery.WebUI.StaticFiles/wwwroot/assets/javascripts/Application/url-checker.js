d9e7e1aea20e56e7f2d3a98335b42ec0a0ca1aba
James Lustig <james.lustig@geeks.ltd.uk>
2020-10-19 17:59:00 +0100

    Add URL checker
    (cherry picked from commit 160f019d63d5b5ad163ea92256540dbed04af9f1)

---
+++ ModernSlavery.WebUI.StaticFiles/wwwroot/assets/javascripts/Application/url-checker.js
@@ -0,0 +1,22 @@
+﻿// ping statement url to check it works
+(function($) {
+    "use strict";
+    
+    $('.').each(function (i, e) {
+    });
+
+    function initialiseUrlCheck($element) {
+        var url = $element.data('statement-url');
+        $.ajax(url, 
+        {
+            success: urlSuccess
+        });
+    };
+
+    function urlSuccess(data, textStatus, jqXHR) {
+    };
+
+    function urlFailed(jqXHR, textStatus, errorThrown) {
+    };
+
+})(jQuery);
