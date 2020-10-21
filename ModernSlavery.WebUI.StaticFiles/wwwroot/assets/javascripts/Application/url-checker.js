f85b453e3907f1b7b3589806a9ed5a88fd8941a7
James Lustig <james.lustig@geeks.ltd.uk>
2020-10-21 16:50:55 +0100

    Investigating CORS and jsonp

--- ModernSlavery.WebUI.StaticFiles/wwwroot/assets/javascripts/Application/url-checker.js
+++ ModernSlavery.WebUI.StaticFiles/wwwroot/assets/javascripts/Application/url-checker.js
@@ -1,22 +1,61 @@
 ﻿// ping statement url to check it works
-(function($) {
+(function ($) {
     "use strict";
-    
-    $('.').each(function (i, e) {
-    });
+
+    $('.app-statement-summary_url')
+        .each(function (i, e) {
+            initialiseUrlCheck($(e));
+        });
 
     function initialiseUrlCheck($element) {
         var url = $element.data('statement-url');
-        $.ajax(url, 
-        {
-            success: urlSuccess
+        console.log(url);
+        //var myheaders = new Headers();
+        //var myrequest = new Request(url, {
+        //    method: 'get',
+        //    mode: 'cors',
+        //    credentials: 'omit'
+        //});
+        //fetch(myrequest)
+        //    .then(response => {
+        //        debugger;
+        //        console.log(url);
+        //        console.log(response);
+        //    });
+
+        $.ajax({
+            url: url,
+            dataType: 'jsonp',
+            mode: 'cors',
+            statusCode: {
+                0: function () {
+                    console.log($element.data('statement-url') + ": status code 0 returned");
+                },
+                200: function () {
+                    console.log($element.data('statement-url') + ": status code 200 returned");
+                },
+                404: function () {
+                    console.log($element.data('statement-url') + ": status code 404 returned");
+                }
+            },
+            success: function (data, textStatus, jqXHR) {
+                console.log($element.data('statement-url') + ": Success");
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log($element.data('statement-url') + ": Error");
+            },
+            complete: function () {
+                console.log($element.data('statement-url') + ": Complete");
+            }
         });
     };
 
     function urlSuccess(data, textStatus, jqXHR) {
+        debugger;
     };
 
     function urlFailed(jqXHR, textStatus, errorThrown) {
+        debugger;
     };
 
 })(jQuery);
