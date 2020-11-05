b0bb4499077d306f837db528f42f75247ff3bfd0
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-11-05 14:14:00 +0000

    Adding footer check test case

---
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Start Page/Footer_Check.cs
@@ -0,0 +1,83 @@
+﻿using Geeks.Pangolin;
+using ModernSlavery.Core.Interfaces;
+using ModernSlavery.Infrastructure.Hosts;
+using ModernSlavery.Testing.Helpers;
+using ModernSlavery.Testing.Helpers.Classes;
+using ModernSlavery.Testing.Helpers.Extensions;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ModernSlavery.Hosts.Web.Tests
+{
+    [TestFixture]
+
+    public class Footer_Check : BaseUITest
+    {
+        public Footer_Check() : base(TestRunSetup.TestWebHost, TestRunSetup.WebDriverService)
+        {
+
+        }
+
+        private bool TestRunFailed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            if (TestRunFailed)
+                Assert.Inconclusive("Previous test failed");
+            else
+                SetupTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed) TestRunFailed = true;
+
+        }
+
+        [Test, Order(2)]
+        public async Task ReturnToRootPage()
+        {
+            SignOutDeleteCookiesAndReturnToRoot(this);            
+
+            await Task.CompletedTask;
+
+        }
+
+        [Test, Order(4)]
+        public async Task CheckLinkVisibility()
+        {
+            ExpectLink("Contact us");
+            ExpectLink("Cookies");
+            ExpectLink("Privacy Policy");
+
+          
+
+            await Task.CompletedTask;
+
+        }
+
+        [Test, Order(6 )]
+        public async Task CheckLinkNavigation ()
+        {
+            ClickLink("Contact us");
+            ExpectHeader("Contact us");
+            ClickLink("Cookies");
+            ExpectHeader("Cookies on Modern slavery statement registry");
+
+            ClickLink("Privacy Policy");
+            ExpectHeader("Privacy Policy");
+
+
+
+            await Task.CompletedTask;
+
+        }
+    } 
+    
+}
