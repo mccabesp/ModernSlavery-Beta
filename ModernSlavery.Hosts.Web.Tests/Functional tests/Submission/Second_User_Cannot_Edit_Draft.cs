25d85d8c2a0f6c82d4f74e37fd2ceecc512c2298
Samuel Donaldson <samuel.donaldson@geeks.ltd.uk>
2020-11-05 10:45:55 +0000

    Finish second user editing draft test case

--- ModernSlavery.Hosts.Web.Tests/Functional tests/Submission/Second_User_Cannot_Edit_Draft.cs
+++ ModernSlavery.Hosts.Web.Tests/Functional tests/Submission/Second_User_Cannot_Edit_Draft.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ModernSlavery.Hosts.Web.Tests
 {
-    [TestFixture, Ignore("Awaiting Submission merge")]
+    [TestFixture]
     public class Second_User_Cannot_Edit_Draft : Create_Account_Add_Second_User
     {
         const string _firstname = Create_Account.roger_first; const string _lastname = Create_Account.roger_last; const string _title = Create_Account.roger_job_title; const string _email = Create_Account.roger_email; const string _password = Create_Account.roger_password;
@@ -26,7 +26,7 @@ namespace ModernSlavery.Hosts.Web.Tests
 
 
         [Test, Order(29)]
-        public async Task RegisterOrg()
+        public async Task RegisterOrgs()
         {
             org = this.Find<Organisation>(org => org.GetLatestActiveScope().ScopeStatus.IsAny(ScopeStatuses.PresumedOutOfScope, ScopeStatuses.PresumedInScope) && org.LatestRegistrationUserId == null && !org.UserOrganisations.Any());
 
@@ -57,6 +57,75 @@ namespace ModernSlavery.Hosts.Web.Tests
             await AxeHelper.CheckAccessibilityAsync(this, httpMethod: "POST");
             ModernSlavery.Testing.Helpers.Extensions.SubmissionHelper.GroupOrSingleScreenComplete(this, OrgName: TestData.OrgName);
 
+            await Task.CompletedTask;
+        }
+
+        [Test, Order(42)]
+        public async Task YourModernSlaveryStatement()
+        {
+
+            ExpectHeader("Your modern slavery statement");
+
+            Set("URL").To(Submission.YourMSStatement_URL);
+
+            Submission_Helper.DateSet(this, Submission.YourMSStatement_To_Day, Submission.YourMSStatement_To_Month, Submission.YourMSStatement_From_Year, "1");
+            Submission_Helper.DateSet(this, Submission.YourMSStatement_To_Day, Submission.YourMSStatement_To_Month, Submission.YourMSStatement_To_Year, "2");
+
+            Set("First name").To(Submission.YourMSStatement_First);
+            Set("Last name").To(Submission.YourMSStatement_Last);
+            Set("Job title").To(Submission.YourMSStatement_JobTitle);
+
+            Submission_Helper.DateSet(this, Submission.YourMSStatement_ApprovalDate_Day, Submission.YourMSStatement_ApprovalDate_Month, Submission.YourMSStatement_ApprovalDate_Year, "3");
+
+            Click("Continue");
+            await AxeHelper.CheckAccessibilityAsync(this, httpMethod: "POST");
+            await Task.CompletedTask;
+        }
+
+        [Test, Order(44)]
+        public async Task SignOutAndContinueDraftAsFirstUser()
+        {
+            Click("Sign out");
+
+            Click(The.Top, "Sign in");
+
+            ExpectHeader("Sign in or create an account");
+            Set("Email").To(UniqueEmail);
+            Set("Password").To(_password);
+
+            Click(The.Bottom, "Sign in");
+
+            ExpectHeader("Select an organisation");
+
+            Click(org.OrganisationName);
+            await AxeHelper.CheckAccessibilityAsync(this, httpMethod: "POST");
+            ExpectHeader(That.Contains, "Manage your modern slavery statement submissions");
+
+
+            Click(The.Top, "Continue");
+
+            await Task.CompletedTask;
+        }
+
+        [Test, Order(46)]
+        public async Task ExpectStatementLockedText()
+        {
+            ExpectHeader("Statement Summary Locked");
+
+            Expect("Another user is currently editing this statement summary. Please try again later");
+
+
+            await AxeHelper.CheckAccessibilityAsync(this, httpMethod: "POST");
+            await Task.CompletedTask;
+        }
+
+        [Test, Order(48)]
+        public async Task ClickingContinueLeadsToSubmissionsPage()
+        {
+            Click("Continue");
+            ExpectHeader(That.Contains, "Manage your modern slavery statement submissions");
+
+            await AxeHelper.CheckAccessibilityAsync(this, httpMethod: "POST");
             await Task.CompletedTask;
         }
     }
