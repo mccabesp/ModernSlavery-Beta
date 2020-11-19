2e8a90190accdcd4c3d34fc8a5c880236428cdd2
James Lustig <james.lustig@geeks.ltd.uk>
2020-11-19 15:15:23 +0000

    Add unselecting checkboxes in statement workflow

---
+++ ModernSlavery.WebUI.StaticFiles/wwwroot/assets/javascripts/Application/mutually-exclusive-checkboxes.js
@@ -0,0 +1,31 @@
+﻿(function ($) {
+    "use strict";
+
+    $('input[type=checkbox][data-mutually-exclusive-group]')
+        .each(function (i, e) {
+            addChangeHandler($(e))
+        });
+
+    function addChangeHandler(element) {
+        element.change(function () {
+
+            var isChecked = element.is(':checked');
+
+            // only do something if checking
+            if (!isChecked)
+                return;
+
+            var name = element.attr('name');
+            var group = element.data('mutually-exclusive-group');
+
+            // find other checked checkboxes sharing the same name
+            var checkedOthers = $('[name=' + name + ']:checked')
+                // not in the same group
+                .not('[data-mutually-exclusive-group=' + group + ']');
+
+            // uncheck them
+            checkedOthers.prop('checked', false);
+        });
+    }
+
+}(jQuery));
